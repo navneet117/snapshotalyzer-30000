6df43d47eed598047aaccd50d4a191a4027496d0
Rana <Navneet.Rana@thomsonreuters.com>
2019-04-05 15:26:40 +0000

    Filter instances by project. Also, stop and satrt instances.

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -4,11 +4,54 @@ import click
 session = boto3.Session(profile_name='shotty')
 ec2 = session.resource('ec2')
 
-@click.command()
-def list_instances():
+def filter_instances(project):
+    instances = []
+
+    if project:
+        filters = [{'Name':'tag:Project', 'Values':["Valkyrie"]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+        return instances
+
+@click.group()
+def instances():
+    """Commands for instances"""
+
+@instances.command('list')
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<nam>)")
+def list_instances(project):
     "List EC2 instances"
-    for i in ec2.instances.all():
-        print(i)
+
+    instances = filter_instances(project)
+    for i in instances:
+        tags = { t['Key']: t['Value'] for t in i.tags or [] }
+        print(', '.join((
+        i.id,
+        i.instance_type,
+        i.placement['AvailabilityZone'],
+        i.state['Name'],
+        i.public_dns_name,
+        tags.get('project','<no project>')
+        )))
+
+    return
+
+@instances.command('stop')
+@click.option('--project', default=None,
+  help='Only instances for project')
+def stop_instances(project):
+    "Stop EC2 instnaces"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+            print("Stopping {0}...".format(i.id))
+            i.stop()
+
+    return
 
 if __name__ == '__main__':
-    list_instances()
+    instances()
